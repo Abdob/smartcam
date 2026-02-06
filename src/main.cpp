72db3a1a20f44734d9011fcd9ec45efebc15f629
wave <abdo.babukr@gmail.com>
2026-02-06 11:19:28 -0800

    update with text overlay

--- src/main.cpp
+++ src/main.cpp
@@ -1,100 +1,106 @@
-/*
- * Copyright 2021 Xilinx Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-#include <glob.h>
-#include <stdio.h>
 #include <gst/gst.h>
-#include <string>
-#include <array>
-#include <vector>
-#include <sstream>
-#include <memory>
-#include <stdexcept>
-#include <unistd.h>
-#include <sys/types.h>
+#include <gst/vvas/gstinferencemeta.h>
+#include <stdlib.h>
+#include <time.h>
+#include <cstdio>
 
-static gboolean my_bus_callback(GstBus *bus, GstMessage *message, gpointer data) {
-    GMainLoop *loop = (GMainLoop *)data;
-    switch (GST_MESSAGE_TYPE(message)) {
-        case GST_MESSAGE_EOS:
-            g_print("End of stream\n");
-            g_main_loop_quit(loop);
-            break;
-        case GST_MESSAGE_ERROR: {
-            GError *err;
-            gchar *debug;
-            gst_message_parse_error(message, &err, &debug);
-            g_printerr("Error: %s\n", err->message);
-            g_free(debug);
-            g_error_free(err);
-            g_main_loop_quit(loop);
-            break;
-        }
-        default:
-            break;
-    }
-    return TRUE;
+/* These mimic your persistent hardware buffers (kernel_priv->mango_pix) */
+static uint32_t g_mango = 0;
+static uint32_t g_defect = 0;
+
+/* Metadata Injection Probe: Replicates the xlnx_kernel_start logic */
+static GstPadProbeReturn
+inject_metadata_probe(GstPad *pad, GstPadProbeInfo *info, gpointer user_data) {
+    GstBuffer *buffer = GST_PAD_PROBE_INFO_BUFFER(info);
+    
+    // Simulate new "FPGA" results
+    g_mango = 50000 + (rand() % 5000);
+    g_defect = rand() % 2000;
+
+    /* --- START OF METADATA ALLOCATION (Similar to your example) --- */
+    
+    // 1. Add the meta container to the buffer
+    GstInferenceMeta *infer_meta = (GstInferenceMeta *) gst_buffer_add_meta (
+                                     buffer, 
+                                     gst_inference_meta_get_info (), 
+                                     NULL);
+
+    // 2. Allocate root prediction
+    infer_meta->prediction = gst_inference_prediction_new ();
+
+    // 3. Allocate child prediction
+    GstInferencePrediction *predict = gst_inference_prediction_new ();
+
+    // 4. Assign pointers to the reserved fields (mimics your hardware pointers)
+    predict->reserved_1 = (void *) &g_mango;
+    predict->reserved_2 = (void *) &g_defect;
+
+    // 5. Create and append classification (required for the overlay loop)
+    GstInferenceClassification *cls = gst_inference_classification_new_full (
+                                        -1, 0.0, "DEFECT DENSITY", 0, 
+                                        NULL, NULL, NULL);
+    gst_inference_prediction_append_classification (predict, cls);
+
+    // 6. Append child to root (Ownership is transferred, recursive free is set)
+    gst_inference_prediction_append (infer_meta->prediction, predict);
+
+    /* --- END OF METADATA ALLOCATION --- */
+
+    return GST_PAD_PROBE_OK;
 }
 
 int main(int argc, char *argv[]) {
-    GMainLoop *loop;
+
+    printf("Stuff got eliminated\n");
     GstElement *pipeline;
     GstBus *bus;
-    guint bus_watch_id;
+    GstMessage *msg;
 
-    /* Initialize GStreamer */
     gst_init(&argc, &argv);
-    loop = g_main_loop_new(NULL, FALSE);
+    srand(time(NULL));
 
-    int width = 1920;
-    int height = 1080;
-    /* The simplified pipeline string */
-    gchar *pip = g_strdup_printf(
-        "v4l2src device=/dev/video0 io-mode=mmap ! h264parse ! video/x-h264,alignment=au ! "
-        "queue ! omxh264dec low-latency=true ! video/x-raw, width=%d, height=%d ! "
-        "tee name=t ! queue ! vvas_xmultisrc kconfig=\"/opt/xilinx/kv260-smartcam/share/vvas/facedetect/preprocess.json\" ! "
-        "queue ! vvas_xfilter kernels-config=\"/opt/xilinx/kv260-smartcam/share/vvas/facedetect/aiinference.json\" ! "
-        "ima.sink_master vvas_xmetaaffixer name=ima ima.src_master ! fakesink "
-        "t. ! queue max-size-buffers=1 leaky=2 ! ima.sink_slave_0 ima.src_slave_0 ! "
-        "queue ! vvas_xfilter kernels-config=\"/opt/xilinx/kv260-smartcam/share/vvas/facedetect/drawresult.json\" ! "
-        "queue ! kmssink driver-name=xlnx plane-id=39 sync=false fullscreen-overlay=true", 
-        width, height);
+    /* Define the GRAY8 pipeline string */
+    const char* pipeline_cmd = 
+        "videotestsrc name=vtestsrc ! "
+        "video/x-raw, width=1280, height=720, format=RGB ! "
+        "vvas_xfilter name=text2overlay kernels-config=\"/opt/xilinx/kv260-defect-detect/share/vvas/text2overlay.json\" ! "
+        "kmssink driver-name=xlnx plane-id=39 sync=false fullscreen-overlay=true";
 
-    /* Launch the pipeline */
-    pipeline = gst_parse_launch(pip, NULL);
+    /* Print the pipeline for verification */
+    printf("\n==================================================\n");
+    printf("DEBUG: Launching GStreamer pipeline:\n%s\n", pipeline_cmd);
+    printf("==================================================\n\n");
+
+    /* Build the pipeline */
+    pipeline = gst_parse_launch(pipeline_cmd, NULL);
+
+
+        
     if (!pipeline) {
-        g_printerr("Failed to create pipeline.\n");
+        g_printerr("Pipeline creation failed. Verify vvas_xfilter is in path.\n");
         return -1;
     }
 
+    /* Get the pad and attach the metadata injector */
+    GstElement *vsrc = gst_bin_get_by_name(GST_BIN(pipeline), "vtestsrc");
+    GstPad *srcpad = gst_element_get_static_pad(vsrc, "src");
+    gst_pad_add_probe(srcpad, GST_PAD_PROBE_TYPE_BUFFER, (GstPadProbeCallback)inject_metadata_probe, NULL, NULL);
+
+    gst_object_unref(srcpad);
+    gst_object_unref(vsrc);
+
+    /* Play */
     gst_element_set_state(pipeline, GST_STATE_PLAYING);
+    g_print("Simulation running. Text2Overlay is now consuming injected metadata.\n");
 
-    /* Add a bus watch to handle messages */
     bus = gst_element_get_bus(pipeline);
-    bus_watch_id = gst_bus_add_watch(bus, my_bus_callback, loop);
+    msg = gst_bus_timed_pop_filtered(bus, GST_CLOCK_TIME_NONE, (GstMessageType)(GST_MESSAGE_ERROR | GST_MESSAGE_EOS));
+
+    /* Shutdown */
+    if (msg != NULL) gst_message_unref(msg);
     gst_object_unref(bus);
-
-    /* Run the loop */
-    g_print("Running simplified pipeline: %s\n", pip);
-    g_main_loop_run(loop);
-
-    /* Clean up */
     gst_element_set_state(pipeline, GST_STATE_NULL);
     gst_object_unref(pipeline);
-    g_source_remove(bus_watch_id);
-    g_main_loop_unref(loop);
 
     return 0;
 }
