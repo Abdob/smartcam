bde154e9bb99a622c472a1a694eb123dff403b2f
wave <abdo.babukr@gmail.com>
2026-02-08 10:54:27 -0800

    update

--- src/main.cpp
+++ src/main.cpp
@@ -4,50 +4,6 @@
 #include <time.h>
 #include <cstdio>
 
-/* These mimic your persistent hardware buffers (kernel_priv->mango_pix) */
-static uint32_t g_mango = 0;
-static uint32_t g_defect = 0;
-
-/* Metadata Injection Probe: Replicates the xlnx_kernel_start logic */
-static GstPadProbeReturn
-inject_metadata_probe(GstPad *pad, GstPadProbeInfo *info, gpointer user_data) {
-    GstBuffer *buffer = GST_PAD_PROBE_INFO_BUFFER(info);
-    
-    // Simulate new "FPGA" results
-    g_mango = 50000 + (rand() % 5000);
-    g_defect = rand() % 2000;
-
-    /* --- START OF METADATA ALLOCATION (Similar to your example) --- */
-    
-    // 1. Add the meta container to the buffer
-    GstInferenceMeta *infer_meta = (GstInferenceMeta *) gst_buffer_add_meta (
-                                     buffer, 
-                                     gst_inference_meta_get_info (), 
-                                     NULL);
-
-    // 2. Allocate root prediction
-    infer_meta->prediction = gst_inference_prediction_new ();
-
-    // 3. Allocate child prediction
-    GstInferencePrediction *predict = gst_inference_prediction_new ();
-
-    // 4. Assign pointers to the reserved fields (mimics your hardware pointers)
-    predict->reserved_1 = (void *) &g_mango;
-    predict->reserved_2 = (void *) &g_defect;
-
-    // 5. Create and append classification (required for the overlay loop)
-    GstInferenceClassification *cls = gst_inference_classification_new_full (
-                                        -1, 0.0, "DEFECT DENSITY", 0, 
-                                        NULL, NULL, NULL);
-    gst_inference_prediction_append_classification (predict, cls);
-
-    // 6. Append child to root (Ownership is transferred, recursive free is set)
-    gst_inference_prediction_append (infer_meta->prediction, predict);
-
-    /* --- END OF METADATA ALLOCATION --- */
-
-    return GST_PAD_PROBE_OK;
-}
 
 int main(int argc, char *argv[]) {
 
@@ -68,7 +24,7 @@ int main(int argc, char *argv[]) {
         "queue ! "
 	    "vvas_xmultisrc kconfig=\"/opt/xilinx/kv260-smartcam/share/vvas/facedetect/nv122bgra.json\" ! "
 	    "video/x-raw, width=1920, height=1080, format=RGBA ! "
-        "queue name=source0 ! "
+        "queue ! "
         "vvas_xfilter name=text2overlay kernels-config=\"/opt/xilinx/kv260-smartcam/share/vvas/text2overlay.json\" ! "
         "queue ! "
         "kmssink bus-id=fd4a0000.display plane-id=40 sync=false can-scale=false";
@@ -87,14 +43,6 @@ int main(int argc, char *argv[]) {
         g_printerr("Pipeline creation failed. Verify vvas_xfilter is in path.\n");
         return -1;
     }
-
-    /* Get the pad and attach the metadata injector */
-    GstElement *vsrc = gst_bin_get_by_name(GST_BIN(pipeline), "source0");
-    GstPad *srcpad = gst_element_get_static_pad(vsrc, "src");
-    gst_pad_add_probe(srcpad, GST_PAD_PROBE_TYPE_BUFFER, (GstPadProbeCallback)inject_metadata_probe, NULL, NULL);
-
-    gst_object_unref(srcpad);
-    gst_object_unref(vsrc);
 
     /* Play */
     gst_element_set_state(pipeline, GST_STATE_PLAYING);
