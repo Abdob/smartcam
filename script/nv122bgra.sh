d1035de21d93b19c46f4cbb98d485e88493d8efc
wave <abdo@thzcorp.com>
2026-02-15 01:12:09 -0800

    did

--- script/nv122bgra.sh
+++ script/nv122bgra.sh
@@ -1,8 +1,12 @@
+# 1 = Manual Mode
+v4l2-ctl -d /dev/video0 -c auto_exposure=1 
+# Set a low value for fast shutter (Experiment with this number)
+v4l2-ctl -d /dev/video0 -c exposure_time_absolute=100
 #######################################################################################
 SOURCE1=
 SOURCE2=
 #######################################################################################
-DISPLAY1="kmssink bus-id=fd4a0000.display plane-id=40 sync=false can-scale=false"
+DISPLAY1="kmssink plane-id=41"
 DISPLAY2="kmssink driver-name=xlnx plane-id=39 sync=false fullscreen-overlay=true"
 RUN=bgr7
 FILE="multifilesink location=$RUN/capture%04d.bgr"
@@ -11,15 +15,20 @@ FILE="multifilesink location=$RUN/capture%04d.bgr"
 mkdir -p $RUN
 W=1920
 H=1080
-gst-launch-1.0 videotestsrc num-buffers=300 ! \
+gst-launch-1.0 v4l2src device=/dev/video0 io-mode=mmap num-buffers=1000 ! \
+	h264parse ! \
+	"video/x-h264,alignment=au" ! \
+	queue ! \
+	omxh264dec low-latency=true ! \
 	"video/x-raw, width=$W, height=$H, format=NV12" ! \
 	queue ! \
+	perf ! \
 	vvas_xmultisrc kconfig="/opt/xilinx/kv260-smartcam/share/vvas/facedetect/nv122bgra.json" ! \
 	video/x-raw, width=$W, height=$H, format=RGBA ! \
 	queue ! \
     vvas_xfilter name=text2overlay kernels-config="/opt/xilinx/kv260-smartcam/share/vvas/text2overlay.json" ! \
     queue ! \
-	$DISPLAY1
+	$DISPLAY1 -v
 
 #######################################################################################
 #scp -r $KV:/home/petalinux/bgr6 ~/Videos/
