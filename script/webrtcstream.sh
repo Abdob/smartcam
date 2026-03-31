8db3b71b5fa38dc9aad6e26e1ee381c6dfcd08ee
wave <abdo@thzcorp.com>
2026-03-31 16:23:42 -0700

    encoder settings

--- script/webrtcstream.sh
+++ script/webrtcstream.sh
@@ -1,4 +1,4 @@
-IP_ADDR=192.168.1.66
+IP_ADDR=192.168.10.198
 #####################################################################################
 DISPLAY1="kmssink plane-id=41 sync=false can-scale=false"
 DISPLAY2="kmssink driver-name=xlnx plane-id=39 sync=false fullscreen-overlay=true"
@@ -6,7 +6,7 @@ DISPLAY2="kmssink driver-name=xlnx plane-id=39 sync=false fullscreen-overlay=tru
 #R_1920x1080 5 | R_1280x720 10 | R_800x600 24 | R_640x480 30
 W=1920
 H=1080
-gst-launch-1.0 v4l2src device=/dev/video0 io-mode=mmap ! \
+gst-launch-1.0 v4l2src device=/dev/video2 io-mode=mmap ! \
 	h264parse ! \
 	"video/x-h264,alignment=au" ! \
 	omxh264dec low-latency=true ! \
@@ -16,7 +16,8 @@ gst-launch-1.0 v4l2src device=/dev/video0 io-mode=mmap ! \
 	vvas_xmultisrc kconfig="/opt/xilinx/kv260-smartcam/share/vvas/facedetect/rgba2nv12.json" ! \
 	video/x-raw, width=$W, height=$H, format=NV12 ! \
 	thzmeta ! \
-	omxh264enc periodicity-idr=60 ! \
+	timeoverlay ! \
+	omxh264enc periodicity-idr=60 target-bitrate=4000 control-rate=low-latency gop-length=30 ! \
 	webrtcsink \
 	forward-metas="thz" \
 	enable-control-data-channel=true \
